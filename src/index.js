afeff0ae9241572a1f7dfdbeefa29182c0978176
tvu2000 <ttvu39400@gmail.com>
2021-03-22 23:12:44 -0700

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<h2>hello</h2>, document.getElementById("navigation")); 
+ReactDOM.render(
+    <App /> ,
+  document.getElementById('root')
+);
+
