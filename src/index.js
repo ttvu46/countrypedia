bee07f1b31cef795e4b4336ab15e91e9ca522249
tvu2000 <ttvu39400@gmail.com>
2021-03-23 22:49:10 -0700

    install Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import NavBar from './NavBar';
 
-ReactDOM.render(<h2>hello</h2>, document.getElementById("navigation")); 
 ReactDOM.render(
-    <App /> ,
+    <NavBar/> ,
   document.getElementById('root')
 );
 
