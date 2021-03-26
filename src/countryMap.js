3a19fa9ff33142e35f4751f917f15b5a4cde8e7a
tvu2000 <ttvu39400@gmail.com>
2021-03-25 17:20:37 -0700

    map hover

--- src/countryMap.js
+++ src/countryMap.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import Overlay from "react-bootstrap/Overlay";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 function Map() {
+  const [mouseOver, setMouseOver] = useState(false);
+
+  function handleMouse() {
+    return setMouseOver(true);
+  }
   return (
     <div>
-      <ComposableMap>
+      <ComposableMap
+        style={{
+          backgroundColor: "#1b1717",
+        }}
+      >
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
+              <Geography
+                onMouseOver={() => {
+                  const countryName = geo.properties.NAME;
+                  console.log(countryName);
+                }}
+                style={{
+                  default: {
+                    fill: "#f0c929",
+                    outline: "none",
+                    stroke: "#FFFFFF",
+                    strokeWidth: "0.9",
+                  },
+                  hover: {
+                    fill: "grey",
+                    stroke: "black",
+                    outline: "#184d47",
+                  },
+                  pressed: {
+                    fill: "#96bb7c",
+                    outline: "none",
+                  },
+                }}
+                key={geo.rsmKey}
+                geography={geo}
+              />
             ))
           }
         </Geographies>
