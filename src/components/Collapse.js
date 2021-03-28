10329870733aeda476b026c42857d923cfbbda1e
tvu2000 <ttvu39400@gmail.com>
2021-03-28 13:45:49 -0700

    bigger flag icon

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -5,11 +5,11 @@ function CollapseCon({ open, setOpen, clickedInfo, countryCode }) {
   let style = "sidebar";
   style += open ? " open" : " close";
   let code = countryCode.toLowerCase();
-  let srcLink = "https://flagcdn.com/24x18/" + code + ".png";
+  let srcLink = "https://flagcdn.com/56x42/" + code + ".png";
   let srcSetLink =
-    "https://flagcdn.com/48x36/" +
+    "https://flagcdn.com/112x84/" +
     code +
-    ".png 2x, https://flagcdn.com/72x54/" +
+    ".png 2x, https://flagcdn.com/168x126/" +
     code +
     ".png 3x";
   return (
@@ -25,7 +25,7 @@ function CollapseCon({ open, setOpen, clickedInfo, countryCode }) {
       </a>
 
       <h1>{clickedInfo}</h1>
-      <img src={srcLink} srcset={srcSetLink} width="24" height="18"></img>
+      <img src={srcLink} srcset={srcSetLink} width="56" height="42"></img>
     </div>
   );
 }
