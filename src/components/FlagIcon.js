ef7085640fbdb46e48fda488c6504ab4c4860290
tvu2000 <ttvu39400@gmail.com>
2021-03-30 00:50:00 -0700

    create FlagIcon.js

---
+++ src/components/FlagIcon.js
@@ -0,0 +1,11 @@
+function flagIcon() {
+  let code;
+  let srcLink = "https://flagcdn.com/56x42/" + code + ".png";
+  let srcSetLink =
+    "https://flagcdn.com/112x84/" +
+    code +
+    ".png 2x, https://flagcdn.com/168x126/" +
+    code +
+    ".png 3x";
+  return {};
+}
